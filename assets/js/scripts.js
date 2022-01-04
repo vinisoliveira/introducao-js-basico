7af68b42962fa958de5b3ab3a6777fe850e8944d
Vinícius Oliveira <vinicius.oliveira1911@gmail.com>
2022-01-04 17:08:11 -0400

    Update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment() {
+    if (currentNumber < 10 ) {
+        currentNumber = currentNumber + 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+}
+}
+
+function decrement() {
+    if (currentNumber > -10 ) {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+}
