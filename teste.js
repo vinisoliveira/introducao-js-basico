d04ac753b2150b404f378aa89d902e39a68f19ef
Vinícius Oliveira <vinicius.oliveira1911@gmail.com>
2022-01-03 20:07:53 -0400

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,13 @@
+function returnEvenValues(array) {
+    let evenNums = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0){
+            evenNums.push(array[i]);
+        }
+    }
+    console.log("Os números pares são", evenNums);
+}
+
+let array = [1,2,3,4,5,6,7,8,9,10];
+
+returnEvenValues(array);
